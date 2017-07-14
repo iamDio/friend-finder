65e695407a765ef3e475329bef34ed0b5601f792
iamDio <claudio.villani00@gmail.com>
2017-07-14 17:20:20 -0400

    updated htmlRouting

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = 3000;
+
+
+require('./app/routing/htmlRoutes.js')(app);
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+app.listen(PORT,() =>{console.log(`App listening on PORT ${PORT}`)});
+
+
+/*
+var publicFolder = __dirname + '/public/';
+config.public_folder = publicFolder;*/
